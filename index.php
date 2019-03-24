179cad3dea7d8cfd2d3eb96ee47253364c3d2d5d
vektorel <cenkcorabay@gmail.com>
2019-03-24 12:22:13 +0200

    yorum satırları eklendi

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php 
-
+//baba naptın
 include('functions/main_functions.php');
-
+//nasıl laaa
 include('inc/header.php');
-
+//yorum satırı
 $mesaj_no = (isset($_GET['mesaj'])) ? $_GET['mesaj'] : 0;
-
+//herkesi takip edemem ki baba
 if(!isset($_GET['main'])){
 	include('inc/main/main.php');
 } else {
